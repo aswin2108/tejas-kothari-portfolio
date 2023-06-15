c14f19f7d140aa2d3ed39bc569b4b73c8ec54647
aswins2108@gmail.com <aswins2108@gmail.com>
2023-06-15 21:15:27 +0530

    Updates

---
+++ precache-manifest.177a99739ed0cde91dcf5eddf427cc24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "24bc61c4edac590214a0",
+    "url": "/tejas-kothari-portfolio/static/css/main.8b991c8a.chunk.css"
+  },
+  {
+    "revision": "24bc61c4edac590214a0",
+    "url": "/tejas-kothari-portfolio/static/js/main.cb08f57d.chunk.js"
+  },
+  {
+    "revision": "d9026c8897d312851146",
+    "url": "/tejas-kothari-portfolio/static/js/runtime~main.4f4b2ef3.js"
+  },
+  {
+    "revision": "348fba211df4e0348019",
+    "url": "/tejas-kothari-portfolio/static/js/2.52360a2a.chunk.js"
+  },
+  {
+    "revision": "f4866491ae15624a43d3",
+    "url": "/tejas-kothari-portfolio/static/js/3.b700955a.chunk.js"
+  },
+  {
+    "revision": "842e320f0e8473e2d7c3c056b00df030",
+    "url": "/tejas-kothari-portfolio/static/media/logo-image.842e320f.png"
+  },
+  {
+    "revision": "26df447c41afd7bcffcf4c677e4bf56f",
+    "url": "/tejas-kothari-portfolio/static/media/Hi.26df447c.gif"
+  },
+  {
+    "revision": "934902e8b9d686108232ceba59ec89f0",
+    "url": "/tejas-kothari-portfolio/index.html"
+  }
+];
